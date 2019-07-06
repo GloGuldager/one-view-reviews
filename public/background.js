9ff31b5a0cd264916e97c2dec54c2d0ec572843b
wabbot <wabbot24@gmail.com>
2019-07-05 21:14:05 -0400

    modal magic part 1

--- public/background.js
+++ public/background.js
@@ -11,11 +11,5 @@ chrome.contextMenus.onClicked.addListener(() => {
     });
 });
 
-// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
-//     // since only one tab should be active and in the current window at once
-//     // the return variable should only have one entry
-//     var activeTab = tabs[0];
-//     var activeTabId = activeTab.id; // or do whatever you need
-
-//  });
+// editable
--- public/content.js
+++ public/content.js
@@ -2,68 +2,100 @@ chrome.runtime.onMessage.addListener(
     function (request, sender) {
         if (request.type === 'analyzeReviews') {
             const rawURL = request.url;
-            console.log(rawURL);
+
+            // this code evaluates the 
 
             if (rawURL.includes('/dp/')) {
                 const path = '/dp/';
-                getASIN(rawURL, path);
+                userInputModal(rawURL, path);
+                // getASIN(rawURL, path, keywords);
             } else if (rawURL.includes('/gp/product/')) {
                 const path = '/gp/product/';
-                getASIN(rawURL, path);
+                userInputModal(rawURL, path);
+                // getASIN(rawURL, path, keywords);
             } else if (rawURL.includes('/product-reviews/')) {
                 const path = '/product-reviews/';
-                getASIN(rawURL, path);
+                userInputModal(rawURL, path);
+                // getASIN(rawURL, path, keywords);
             } else {
                 alert("This extension gives results for one Amazon product at a time. Navigate to a product page and try again!")
             }
-
-
-
-
         }
     });
 
-function getASIN(rawURL, path) {
+
+function userInputModal(rawURL, path) {
+    const modal = document.createElement('dialog');
+    modal.setAttribute("style", "height:350px" );
+    modal.innerHTML =
+        `<iframe id="keywordInput" style="height:100%;"></iframe>
+            <div style="position:absolute; top:1px; left:1px; padding: 3px;">  
+                <button style="background-color: #f68c1e; color: white; font-size: 14px; font-weight: bold;">x</button>
+            </div>
+            <div style="position:absolute; top: 10px; left:5px; margin: 5px; padding: 0px 20px">
+                <img src="https://doc-10-0o-docs.googleusercontent.com/docs/securesc/pgq6srcuslbp8nf1ljrbaamsdu1s4gmd/ucsqfnqomdsc2s608guhmgf1k5tqs3fr/1562364000000/14303230788414761217/14303230788414761217/1329K8Ttgo5QkypVhld8ekkZ4sn_3uOIE?e=view&nonce=uglso8m5e2m5i&user=14303230788414761217&hash=534ni81urr8kuj07mie4iea21qpgsing" alt="OneView Logo">
+            </div>
+            <div style="position:absolute; top: 130px; left:5px;">
+                <div style="margin: 2px 5px;">Add target words or phrases to refine your review search (optional):</div> 
+                <form> 
+                <input style="margin: 5px;" type="text" name="first" id="1" class="input" value=""><br>
+                <input style="margin: 5px;" type="text" name="second" id="2" class="input" value=""><br>
+                <input style="margin: 5px;" type="text" name="third" id="3" class="input" value=""><br>
+                <input style="margin: 5px; padding: 5px 12px; background-color: #f68c1e; color: white; font-size: 16px;" type="submit" value="Analyze">
+                </form>
+            </div>`;
+    document.body.appendChild(modal);
+    const dialog = document.querySelector("dialog");
+    dialog.showModal();
+
+    const iframe = document.getElementById("keywordInput");
+    iframe.frameBorder = 0;
+
+    dialog.querySelector("form").addEventListener("submit", () => {
+        const input1 = document.getElementById('1').value;
+        const input2 = document.getElementById('2').value;
+        const input3 = document.getElementById('3').value;
+        [];
+        if (input1) {
+            keywords.push(input1);
+        }
+        if (input2) {
+            keywords.push(input2);
+        }
+        if (input3) {
+            keywords.push(input3);
+        }
+        getASIN(rawURL, path, keywords);
+    });
+
+    dialog.querySelector("button").addEventListener("click", () => {
+        dialog.close();
+    });
+}
+
+function getASIN(rawURL, path, keywords) {
     const splitASIN = rawURL.split(path)[1];
     const ASIN = splitASIN.substring(0, 10);
     console.log(ASIN);
-
-    createQueryURLs(ASIN);
+    alert(keywords);
+    // createQueryURLs(ASIN);
+    AJAXRequest(ASIN, keywords)
 }
 
-function createQueryURLs(ASIN) {
-    alert(`This product's ASIN code is ${ASIN}`)
+function AJAXRequest(ASIN, keywords) {
+    $.ajax({
+        type: "POST",
+        url: "http://one-view-reviews-api.herokuapp.com/post",
+        data: {"ASIN": ASIN,
+               "keywords": keywords},
+        success: parseData(data),
+        dataType: "json"
+    });
+}
+
+function parseData(data) {
+    alert("data came back!")
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const modal = document.createElement('dialog');
-// modal.setAttribute("style", "height:40%");
-// modal.innerHTML =
-//     `<iframe id="reviewAnalyzer" style="height:100%"></iframe>
-//         <div style="position:absolute; top:0px; left:5px;">  
-//             <button>x</button>
-//         </div>`;
-// document.body.appendChild(modal);
-// const dialog = document.querySelector("dialog");
-// dialog.showModal();
-
-// const iframe = document.getElementById("reviewAnalyzer");  
-// iframe.src = chrome.extension.getURL("index.html");
-// iframe.frameBorder = 0;
-
-// dialog.querySelector("button").addEventListener("click", () => {
-//     dialog.close();
-//  });
